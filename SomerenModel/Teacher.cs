94185baaf69d1160c774b1ccc93bb1c500b741a6
DipikaBhandari <bdipika076@gmail.com>
2023-03-14 10:43:20 +0100

    Added a comment

--- SomerenModel/Teacher.cs
+++ SomerenModel/Teacher.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }     // database id
         public string Name { get; set; }
         public int Number { get; set; } // LecturerNumber, e.g. 47198
+        // HELLO
     }
 }
